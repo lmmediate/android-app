59472c50601aa7b370cab3dcfbb965439970f1ce
Kirill Kupriyanov <kupriyanovkirill@gmail.com>
2018-02-25 15:19:55 +0300

    Layouts for home, favorite and shoplist fragments.

---
+++ app/src/main/java/com/hes/easysales/easysales/ShopListFragment.java
@@ -0,0 +1,24 @@
+package com.hes.easysales.easysales;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by sinopsys on 2/25/18.
+ */
+
+public class ShopListFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_shoplist, container, false);
+    }
+}
+
+
+// EOF
