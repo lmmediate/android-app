e73eb13ed011d01173995d6920a3c679e5242be1
Kirill Kupriyanov <kupriyanovkirill@gmail.com>
2018-03-16 17:15:42 +0300

    Config file for constant app values.

---
+++ app/src/main/java/com/hes/easysales/easysales/Config.java
@@ -0,0 +1,19 @@
+package com.hes.easysales.easysales;
+
+/**
+ * Created by sinopsys on 3/16/18.
+ */
+
+public class Config {
+
+    public static final String URL_CORE = "http://46.17.44.125:8080";
+    public static final String URL_LOGIN = URL_CORE + "/auth/login";
+    public static final String KEY_USERNAME = "username";
+    public static final String KEY_PASSWORD = "password";
+    public static final String TAG_VOLLEY_ERROR = "VOLLEY";
+    public static final String REQUESTS_CONTENT_TYPE = "application/json; charset=utf-8";
+    public static final String BAD_API_AUTH_RESPONSE = "Wrong username/password.";
+}
+
+
+// EOF
